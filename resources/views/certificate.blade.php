f4fe9af3f48f0bf0cb9be23cf99eae0c371a6b71
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-03-31 20:41:10 +0530

    certificate css added

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -1,26 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cerificate</title>
 </head>
+
 <body>
     <div class="certificate">
+        <img src="https://api.itasinc.in/template/eOWbVUrE6cNZD90I38AtVk6K75zs1zK3T7Fq6eee.png">
+        <div class="qr">
+            <p style="text-align: center; font-size: 12px; margin-bottom: 4px;">Scan QR Code to verify certificate</p>
+            {{QrCode::size(100)->generate('https://certificate.itasinc.in/verify?cert_id=cert_id')}}
+        </div>
+        <h1 class="user_name">User Name</h1>
+        <h5 class="event_name">Event Name</h5>
     </div>
-    <div class="">
-    {{QrCode::size(100)->generate('https://google.com')}}
-</div>
-    {{-- <img src="" height="" alt=""> --}}
 </body>
+
 </html>
 
 <style>
-.certificate {
-  width: 100vw;
-  height: 100vh;
-  background: url("https://i.pinimg.com/736x/11/56/79/11567993d33bc440f521f1e868f187db.jpg") no-repeat;
-  background-size: 100% 100%;
-     }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .certificate {
+        position: relative;
+    }
+
+    .qr {
+        position: absolute;
+        top: 700px;
+        left: 800px;
+        width: 100px;
+        background-color: #FFF;
+        color: #000;
+    }
+
+    .user_name {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        font-size: 48px;
+    }
+
+    .event_name {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        font-size: 24px;
+    }
 </style>
