4b650eb16ea0ca32d7a7607815c298a05a84ac3c
Rishi <rk3141508@gmail.com>
2024-04-05 13:15:21 +0530

    redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect(config('app.frontend_url').'/admin/dashboard');
             }
         }
 
