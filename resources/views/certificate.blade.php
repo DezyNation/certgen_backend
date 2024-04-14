089f7b82f9e381a6e68fa1b077fb5814bf9ee524
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-04-14 23:05:37 +0530

    font

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cerificate</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -67,7 +67,5 @@
         left: {{$event_x}}px;
         font-size: {{$event_size}}px;
         color: {{$event_name_color}};
-        font-family: 'Poppins', sans-serif;
-        font-weight: 500;
     }
 </style>
