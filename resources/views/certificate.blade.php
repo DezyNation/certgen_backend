d29a035346040ba1bc78e6c6a5659aa567341807
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-04-14 13:47:18 +0530

    blade

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -12,8 +12,7 @@
     <div class="certificate">
         <img src={{ asset($image) }}>
         <div class="qr">
-            <p style="text-align: center; font-size: 12px; margin-bottom: 4px;">Scan QR Code to verify certificate</p>
-            <img src="data:image/png;base64, {{ $qrcode}}" style="width: {{$qr_size}}px;" alt="QR Code">
+            <img src="data:image/png;base64, {{ $qrcode}}" style="width: {{$qr_size}}px; margin: 2px auto;" alt="QR Code">
             <p style="text-align: center; font-size: 12px; margin-top: 4px; text-transform: uppercase;">{{$certificate_id}}</p>
         </div>
         <h1 class="user_name">{{$user_name}}</h1>
