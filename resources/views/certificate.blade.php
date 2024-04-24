650e320a4670e6fab41a3eb077bf39f9a29cf02b
Rishi <rk3141508@gmail.com>
2024-04-24 19:32:12 +0530

    strg

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -28,15 +28,11 @@
 </html>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
     @page {
         size: 1500px 1061px;
     }
-
-    @font-face {
-        font-family: 'Poppins-Medium';
-        src: url('https://api.itasinc.in/Poppins-Medium.ttf');
-    }
-
     * {
         margin: 0;
         padding: 0;
@@ -71,6 +67,9 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
+        font-family: "Poppins", sans-serif;
+        font-weight: 500;
+        font-style: normal;
     }
 
     .user_name>h1 {
